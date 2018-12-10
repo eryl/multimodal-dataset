9c1b3a093e685e0a316cd7244c37091f0bc5c444
Erik Ylipää <erik.ylipaa@gmail.com>
2018-12-10 12:52:33 +0100

    Removed irrelevant files

--- multimodal/torch/videodataset.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.utils.data
-
-import multimodal.dataset.video
-
-class TorchVideoDataset(torch.utils.data.Dataset):
-    def __init__(self, *video_stores):
-        self.video_stores = video_stores
-        
