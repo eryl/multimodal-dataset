75c0e1aa74216a33387559c4664def4f7461b084
Erik Ylipää <erik.ylipaa@gmail.com>
2019-03-01 07:34:59 +0100

    Refactored the VAD functions to a package module

--- bin/add_vad_signal.py
+++ bin/add_vad_signal.py
@@ -2,139 +2,42 @@
 Goes through a dataset and add dataset to audio tracks with Voice Activity Detection data.
 """
 import multiprocessing.dummy as multiprocessing
-import time
 import os.path
 import argparse
-import queue
-import webrtcvad
 import numpy as np
 import scipy.io.wavfile as wavefile
-from google.cloud import speech
 
-
-from multimodal.dataset.video import VideoDataset
-
-
+from multimodal.dataset.add_vad_signal import add_voiced_segment_facet
 
 def main():
     parser = argparse.ArgumentParser(description="Script for finding speech in the audio "
                                                  "modality of multimodal datasets")
-    parser.add_argument('dataset', help="Datasets to process", nargs='+')
+    parser.add_argument('datasets', help="Datasets to process", nargs='+')
     parser.add_argument('--mode',
                         help="WEBRTC VAD mode, 0 gives most false positives, 3 gives most false negatives",
                         type=int,
                         choices=(0,1,2,3),
-                        default=0)
-    parser.add_argument('--language-code', help="What language code to use for transcription", default='en-GB')
-    parser.add_argument('--quota', help="Maximum number of requests per minute", type=int, default=300)
+                        default=3)
+    parser.add_argument('--n-processes', type=int, default=1)
     args = parser.parse_args()
-    vad = webrtcvad.Vad()
-    vad.set_mode(args.mode)
-
-    for dataset_path in args.dataset:
-        with VideoDataset(dataset_path) as dataset:
-            sample_rate = dataset.get_samplerate('audio')
-            frame_duration = 30
-            n_frame_samples = (sample_rate//1000)*frame_duration
-            speech_frames = []
-            for times, audio_segment, in dataset.get_subtitled_complement_streams(['time', 'audio']):
-                n_frames = len(audio_segment) // n_frame_samples
-                start_time, end_time = times
-                start_sample = int(start_time*sample_rate)
-                for i in range(n_frames):
-                    start = i*n_frame_samples
-                    end = start + n_frame_samples
-                    frame_samples = audio_segment[start:end]
-                    if vad.is_speech(frame_samples.tobytes(), sample_rate):
-                        speech_frames.append((start_sample+start, start_sample+end, frame_samples))
-            if speech_frames:
-                speech_segments = []
-                current_frames = []
-                current_start, current_end, current_sample = speech_frames[0]
-                current_frames.append(current_sample)
-                for frame_start, frame_end, sample in speech_frames[1:]:
-                    if current_end+1 >= frame_start:
-                        current_end = frame_end
-                        current_frames.append(sample)
-                    else:
-                        speech_segments.append((current_start, current_end, np.concatenate(current_frames)))
-                        current_frames = [sample]
-                        current_start = frame_start
-                        current_end = frame_end
-                speech_segments.append((current_start, current_end, np.concatenate(current_frames)))
-
-                audio = np.concatenate([segment for start, end, segment in speech_segments])
-                filename = os.path.splitext(dataset_path)[0] + '.wav'
-                wavefile.write(filename, 16000, audio)
-
-                #transcriptions = transcribe_sync(speech_segments, sample_rate, quota=args.quota, language_code=args.language_code)
-
-                #transcript_file = os.path.splitext(dataset_path)[0] + '.csv'
-                #with open(transcript_file, 'w') as fp:
-                #    fp.write("#start\tend\tconfidence\ttranscript\n")
-                #    for transcript in transcriptions:
-                #        fp.write("{}\t{}\t{}\t{}\n".format(*transcript))
-
-
-def transcribe_sync(speech_segments, sample_rate, quota=300, language_code='en-GB', n_processes=30):
-    # We have a maximum qouta of 300 requests per minute, for now we just run few enough processes for it not to matter
-    speech_segment_queue = multiprocessing.Queue()
-    transcription_queue = multiprocessing.Queue()
-    for speech_segment in speech_segments:
-        speech_segment_queue.put(speech_segment)
-
-    wait_time = 60*n_processes/quota
-    processes = [multiprocessing.Process(target=transcribe_worker,
-                                         args=(speech_segment_queue,
-                                               transcription_queue,
-                                               sample_rate,
-                                               language_code,
-                                               wait_time))
-                 for i in range(n_processes)]
-    for p in processes:
-        p.start()
-    transcriptions = []
-    for i in range(len(speech_segments)):
-        result = transcription_queue.get(True)
-        if result is not None:
-            transcriptions.append(result)
-
-    transcriptions.sort()
-    return transcriptions
-
-
-def transcribe_worker(speech_segment_queue, transcription_queue, sample_rate, language_code, wait_time):
-    client = speech.SpeechClient()
-    config = speech.types.RecognitionConfig(
-        encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
-        language_code=language_code,
-        sample_rate_hertz=sample_rate)
-
-    try:
-        while True:
-            t0 = time.time()
-            (start, end, speech_segment) = speech_segment_queue.get(True, 1)
-            print("Processing segment starting at {}, ending at {}".format(start, end))
-            content = speech_segment.tobytes()
-            audio = speech.types.RecognitionAudio(content=content)
-            response = client.recognize(config=config, audio=audio)
-            results = response.results
-            if results:
-                alternative = response.results[0].alternatives[0]
-                transcription_queue.put((start, end, alternative.confidence, alternative.transcript))
-            else:
-                transcription_queue.put(None)
-            t1 = time.time()
-            if t1 < t0 + wait_time:
-                time.sleep((t0+wait_time) - t1)
-    except queue.Empty:
-        pass
+    if args.n_processes > 1:
+        with multiprocessing.Pool(args.n_processes) as pool:
+            for dataset_path in args.datasets:
+                pool.apply_async(add_voiced_segment_facet, (dataset_path, args.mode))
+            pool.close()
+            pool.join()
+    else:
+        for dataset_path in args.datasets:
+            add_voiced_segment_facet(dataset_path, args.mode)
 
 
 
-
-
-
+def save_waves(voiced_frames, sample_rate, dataset_path):
+    for start, end, audio in voiced_frames:
+        filename = os.path.splitext(dataset_path)[0] + '{:.03f}-{:.03f}.wav'.format(start/sample_rate, end/sample_rate)
+        wavefile.write(filename, 16000, audio)
+    filename = os.path.splitext(dataset_path)[0] + '.wav'
+    wavefile.write(filename, 16000, np.concatenate([audio for start, end, audio in voiced_frames]))
 
 
 if __name__ == '__main__':
---
+++ multimodal/dataset/add_vad_signal.py
@@ -0,0 +1,92 @@
+import collections
+import webrtcvad
+import numpy as np
+
+from multimodal.dataset.video import VideoDataset
+
+
+def add_voiced_segment_facet(dataset_path, vad_mode=3, frame_duration_ms=30, overwrite=False):
+    vad = webrtcvad.Vad()
+    vad.set_mode(vad_mode)
+
+    with VideoDataset(dataset_path, mode='r+') as dataset:
+        audio_facets, = dataset.get_all_facets(['audio'])
+        for audio_facet in audio_facets:
+            if overwrite or not audio_facet.has_time_intervals('voiced_segments'):
+                sample_rate = audio_facet.get_samplerate()
+                voiced_frames = list(vad_slice_audio_signal(audio_facet.get_all_frames(), sample_rate, vad, frame_duration_ms))
+                voiced_frame_intervals = np.array([[start, end] for start, end, segment in voiced_frames])
+                audio_facet.add_time_intervals('voiced_segments', voiced_frame_intervals, overwrite=overwrite)
+            else:
+                print("Audio facet {} already has voiced segment times".format(audio_facet.group_name()))
+
+
+def vad_slice_audio_signal(audio_frames, sample_rate, vad, frame_duration_ms=30, padding_duration_ms = 100):
+    """
+    Apply the VAD classifier to all audio frames and return a list of start, end tuples for each active region detected.
+    :param audio_frames: A numpy array of PCM audio samples
+    :param sample_rate: The sample rate of the audio sampels
+    :return:
+    """
+
+    frame_length = frame_duration_ms * sample_rate//1000
+
+    num_padding_frames = int(padding_duration_ms / frame_duration_ms)
+    # We use a deque for our sliding window/ring buffer.
+    ring_buffer = collections.deque(maxlen=num_padding_frames)
+    # We have two states: TRIGGERED and NOTTRIGGERED. We start in the
+    # NOTTRIGGERED state.
+    triggered = False
+
+    num_windows = len(audio_frames) // frame_length  # We will throw away the last samples less than 30 ms, this should not be an issue
+
+    voiced_frames = []
+    voiced_start_frame = None
+
+    for i in range(num_windows):
+        start = i*frame_length
+        end = start+frame_length
+        audio = audio_frames[start:end]
+        is_speech = vad.is_speech(audio.tobytes(), sample_rate)
+
+        if not triggered:
+            ring_buffer.append((audio, is_speech))
+            num_voiced = len([f for f, speech in ring_buffer if speech])
+            # If we're NOTTRIGGERED and more than 90% of the frames in
+            # the ring buffer are voiced frames, then enter the
+            # TRIGGERED state.
+            if num_voiced > 0.9 * ring_buffer.maxlen:
+                triggered = True
+
+                # We want to yield all the audio we see from now until
+                # we are NOTTRIGGERED, but we have to start with the
+                # audio that's already in the ring buffer.
+                for f, s in ring_buffer:
+                    voiced_frames.append(f)
+
+                # We calculate where these voiced frames start so we can calculate language offsets correctly
+                buffer_length_frames = len(ring_buffer)*frame_length
+                voiced_start_frame = start - buffer_length_frames
+                ring_buffer.clear()
+        else:
+            # We're in the TRIGGERED state, so collect the audio data
+            # and add it to the ring buffer.
+            voiced_frames.append(audio)
+            ring_buffer.append((audio, is_speech))
+            num_unvoiced = len([f for f, speech in ring_buffer if not speech])
+            # If more than 90% of the frames in the ring buffer are
+            # unvoiced, then enter NOTTRIGGERED and yield whatever
+            # audio we've collected.
+            if num_unvoiced > 0.9 * ring_buffer.maxlen:
+                triggered = False
+                concatenated = np.concatenate(voiced_frames)
+                voiced_end_frame = voiced_start_frame + len(concatenated)
+                yield (voiced_start_frame, voiced_end_frame, concatenated)
+                ring_buffer.clear()
+                voiced_frames = []
+    # If we have any leftover voiced audio when we run out of input,
+    # yield it.
+    if voiced_frames:
+        concatenated = np.concatenate(voiced_frames)
+        voiced_end_frame = voiced_start_frame + len(concatenated)
+        yield (voiced_start_frame, voiced_end_frame, concatenated)
