0dfa69f86590b9daf8d47d1e23a19f72ae0e7fc6
Erik Ylipää <erik.ylipaa@gmail.com>
2019-03-07 08:02:54 +0100

    Fixed the multiple-string subtitles

--- multimodal/dataset/facet/subtitle_facet.py
+++ multimodal/dataset/facet/subtitle_facet.py
@@ -2,11 +2,16 @@ import numpy as np
 from multimodal.dataset.facet.facet_handler import FacetHandler
 from numbers import Integral
 
+NULL_COLOR = (-1, -1, -1)
+
 class SubtitleFacet(FacetHandler):
     def __init__(self, *args, **kwargs):
         super(SubtitleFacet, self).__init__(*args, **kwargs)
-        self.texts = self.facetgroup['texts']
+        self.strings = self.facetgroup['strings']
+        self.string_index = self.facetgroup['string_index']
         self.times = self.facetgroup['times']
+        self.string_styles = self.facetgroup['string_styles']
+        self.string_colors = self.facetgroup['string_colors']
 
     @classmethod
     def create_facets(cls, subtitles_modality, subtitles_paths):
@@ -22,69 +27,102 @@ class SubtitleFacet(FacetHandler):
         import h5py
 
         class SubtitleParser(html.parser.HTMLParser):
+
             def __init__(self):
                 super(SubtitleParser, self).__init__()
-                self.color = None
-                self.data = None
+                self.colors = []
+                self.strings = []
+                self.attributes = []
+                self.color = NULL_COLOR
+                self.underline = False
+                self.bold = False
+                self.italic = False
+
 
             def handle_starttag(self, tag, attrs):
                 if tag == 'font':
-                    if attrs[0][0] == 'color':
-                        color = attrs[0][1].strip('#')
-                        if self.color is None:
-                            self.color = []
-                        self.color.append((int(color[:2], 16), int(color[2:4], 16), int(color[4:], 16)))
+                    for (attr, value) in attrs:
+                        if attr == 'color':
+                            color = value.strip('#')
+                            self.color = (int(color[:2], 16), int(color[2:4], 16), int(color[4:], 16))
+                elif tag == 'u':
+                    self.underline = True
+                elif tag == 'b':
+                    self.bold = True
+                elif tag == 'i':
+                    self.italic = True
 
             def handle_data(self, data):
-                if self.data is None:
-                    self.data = []
-                self.data.append(data)
+                self.strings.append(data)
+                self.colors.append(self.color)
+                self.attributes.append(dict(b=self.bold, u=self.underline, i=self.italic))
+
+            def handle_endtag(self, tag):
+                if tag == 'font':
+                    self.color = NULL_COLOR
+                elif tag == 'u':
+                    self.underline = False
+                elif tag == 'b':
+                    self.bold = False
+                elif tag == 'i':
+                    self.italic = False
 
             def wipe(self):
-                self.data = None
-                self.color = None
+                self.colors.clear()
+                self.strings.clear()
+                self.attributes.clear()
+                self.color = NULL_COLOR
+                self.underline = False
+                self.bold = False
+                self.italic = False
 
         with open(subtitles_file) as fp:
             subtitles = list(srt.parse(fp.read()))
         subtitle_parser = SubtitleParser()
 
-        font_colors = dict()
-        color_i = 0
-        all_colors = []
-        all_texts = []
-        times = []
+        ## Each subtitle can contain multiple font-tagged elements. Since we want to preserve the font-information, we
+        ## need to save the potentially multiple subtitles as independent strings. We solve this by using two layers of
+        ## indexing. Each font-tagged string is a separate string in the subtitles dataset, this means that it will have
+        ## equal or more rows than the intervals. We use a separate index to mark which strings belong to each subtitle
+        ## interval using start and end indices into the strings dataset.
 
+        subtitle_times = []  # These are the subtitle times as start and end intervals
+        subtitle_string_index = []  # These are start and end indices into the subtitle_strings dataset.
+        # Its shape is (len(subtitle_times), 2)
+        subtitle_strings = []
+        subtitle_strings_colors = []  # These are the font colors for each of the strings
+        subtitle_strings_styles = []  # These are other attributes (underline, italic or bold)
+        start_subtitle = 0
         for subtitle in subtitles:
             subtitle_parser.wipe()
             start = subtitle.start.total_seconds()
             end = subtitle.end.total_seconds()
-            subtitle_parser.feed(subtitle.content)
-            texts = subtitle_parser.data
-            colors = subtitle_parser.color
-            color_ids = []
-            for text in texts:
-                if colors is not None:
-                    for color_tuple in colors:
-                        if not color_tuple in font_colors:
-                            font_colors[color_tuple] = color_i
-                            color_i += 1
-                        color_id = font_colors[color_tuple]
-                else:
-                    color_id = -1
-                color_ids.extend([color_id, len(text)])
+            subtitle_times.append((start, end))
 
-            all_texts.append(''.join(texts))
-            all_colors.extend(color_ids)
-            times.append([start, end])
-        subtitles_color_index = np.array([color for color, i in sorted(font_colors.items(), key=lambda x: x[1])],
-                                         np.uint8)
+            subtitle_parser.feed(subtitle.content)
+
+            subtitle_strings.extend(subtitle_parser.strings)
+            subtitle_strings_colors.extend(subtitle_parser.colors)
+            subtitle_strings_styles.extend(subtitle_parser.attributes)
+            subtitle_string_index.append((start_subtitle, start_subtitle+len(subtitle_parser.strings)))
+            start_subtitle += len(subtitle_parser.strings)
+
         subtitles_facet = modality_group.require_group(name)
-        subtitles_facet.create_dataset('color_index', data=subtitles_color_index)
-        subtitles_facet.create_dataset('colors', data=np.array(all_colors, dtype=np.int32))
-        subtitle_texts = subtitles_facet.create_dataset('texts', shape=(len(all_texts),),
-                                                             dtype=h5py.special_dtype(vlen=str))
-        subtitle_texts[:] = all_texts
-        subtitles_facet.create_dataset('times', data=np.array(times, dtype=np.float32))
+
+        subtitles_facet.create_dataset('times', data=np.array(subtitle_times, dtype=np.float32))
+        subtitles_facet.create_dataset('string_index', data=np.array(subtitle_string_index, dtype=np.uint32))
+
+        subtitle_texts = subtitles_facet.create_dataset('strings',
+                                                        shape=(len(subtitle_strings),),
+                                                        dtype=h5py.special_dtype(vlen=str))
+        subtitle_texts[:] = subtitle_strings
+
+        arr_subtitle_strings_colors = np.array(subtitle_strings_colors, dtype=np.int16)
+        subtitles_facet.create_dataset('string_colors', data=arr_subtitle_strings_colors)
+
+        arr_subtitle_string_styles = np.array([(style['b'], style['i'], style['u']) for style in subtitle_strings_styles], dtype=np.bool)
+        styles_dataset = subtitles_facet.create_dataset('string_styles', data=arr_subtitle_string_styles)
+
         subtitles_facet.attrs['FacetHandler'] = 'SubtitleFacet'
         return SubtitleFacet(subtitles_facet)
 
@@ -103,14 +141,16 @@ class SubtitleFacet(FacetHandler):
     def __getitem__(self, item):
         if isinstance(item, slice):
             times = self.times[item]
-            texts = self.texts[item]
-            return zip(times, texts)
+            string_indices = self.string_index[item]
+            strings = self.strings[string_indices]
+            return zip(times, strings)
         elif isinstance(item, Integral):
             if item >= len(self.times):
                 raise IndexError()
             time = self.times[item]
-            text = self.texts[item]
-            return time, text
+            string_indices = self.string_index[item]
+            strings = self.strings[string_indices]
+            return time, strings
         else:
             raise TypeError("Invalid argument type.")
 
@@ -126,13 +166,14 @@ class SubtitleFacet(FacetHandler):
         filtered = []
         for i in range(len(self.times)):
             times = self.times[i]
-            text = self.texts[i]
-            if filter(times, text):
-                filtered.append(times)
+            start_string, end_string = self.string_index[i]
+            strings = [string for string in self.strings[start_string : end_string] if filter(string)]
+            if strings:
+                filtered.append((times, '\n'.join(strings)))
         return np.array(filtered)
 
     def get_texts(self):
-        return self.texts[:]
+        return self.strings[:]
 
     def get_subrip_texts(self):
         """
@@ -144,7 +185,30 @@ class SubtitleFacet(FacetHandler):
         subtitle_entries = []
         for i in range(len(self.times)):
             start, end = self.times[i]
-            text = self.texts[i]
+            start_string, end_string = self.string_index[i]
+            strings = []
+            for j in range(start_string, end_string):
+                opening_tags = []
+                closing_tags = []
+                string = self.strings[j]
+                string_color = self.string_colors[j]
+                (bold, italic, underlined) = self.string_styles[j]
+                if np.all(string_color != NULL_COLOR):
+                    hex_color_string = ''.join(['{:02x}'.format(c) for c in string_color])
+                    opening_tags.append('<font color="#{}">'.format(hex_color_string))
+                    closing_tags.append('</font>')
+                if bold:
+                    opening_tags.append('<b>')
+                    closing_tags.append('</b>')
+                if italic:
+                    opening_tags.append('<i>')
+                    closing_tags.append('</i>')
+                if underlined:
+                    opening_tags.append('<u>')
+                    closing_tags.append('</u>')
+                formatted_string = ''.join(opening_tags) + string + ''.join(reversed(closing_tags))
+                strings.append(formatted_string)
+            text = '\n'.join(strings)
             entry = srt.Subtitle(i+1, start=timedelta(seconds=float(start)), end=timedelta(seconds=float(end)), content=text)
             subtitle_entries.append(entry)
         return srt.compose(subtitle_entries)
